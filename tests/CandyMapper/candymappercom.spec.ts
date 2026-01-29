d77b200f2568bdd2057a98af54abf526f3a46ca2
Alejandro Molina <alexmdlnyc@gmail.com>
2026-01-28 22:34:26 -0600

    test: overlay locator more generalized, improved solution for submit button click

--- tests/CandyMapper/candymappercom.spec.ts
+++ tests/CandyMapper/candymappercom.spec.ts
@@ -4,23 +4,24 @@ test.describe('Testing CandyMapper.com Site', () => {
   test('Pop-Up Challenge', async ({ page }) => { 
     
     await page.goto('http://www.candymapper.com');
-    
-    //This timeout is necessary for the page to load all the javascript to load
-    //the event handlers for the submit button
-    await page.waitForTimeout(7000);
 
     //Close the layer and enter first and last name on the page form
     await expect(page.locator('[data-aid="POPUP_MODAL"]')).toBeVisible();
-    await page.locator('#popup-widget307423-close-icon').click();
+    await page.locator('[id^="popup-widget"][id$="-close-icon"]').click();
     await page.locator('input[data-aid="First Name"]').fill('Test');
     await page.locator('input[data-aid="Last Name"]').fill('Name');
    
     //Confirm an error message displays with email missing on submit
     const submitBtn = page.getByRole('button', { name: /submit/i });
-    await submitBtn.click();
-    await expect(page.locator('[data-aid="CONTACT_EMAIL_ERR_REND"]')).toBeVisible();
-    await expect(page.getByText('Please enter a valid email address.')).toBeVisible();
-
+    
+    //Will loop through the error message assertions below until they pass or it times out
+    //Necessary since the page takes time to attach JavaScript event listeners to the submit button
+    await expect(async () => {
+      await submitBtn.click();
+      await expect(page.locator('[data-aid="CONTACT_EMAIL_ERR_REND"]')).toBeVisible();
+      await expect(page.getByText('Please enter a valid email address.')).toBeVisible();
+    }).toPass({ timeout: 10000 });
+    
     //Successfully submit after email is entered
     await page.locator('input[data-aid="CONTACT_FORM_EMAIL"]').fill('email@email.com');
     await submitBtn.click();
